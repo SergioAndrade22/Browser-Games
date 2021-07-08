482c5177b7bf2eae0a0e9851c019d8bc5f764fdb
SergioAndrade22 <sergio.david2201@gmail.com>
2021-07-08 13:54:06 -0300

    Added basic movement and game logic

--- mazmorra/js/juego.js
+++ mazmorra/js/juego.js
@@ -21,7 +21,7 @@ var escenario = [
   [0,2,2,0,0,0,0,2,0,0,0,2,0,0,0],
   [0,0,2,0,0,0,2,2,2,0,0,2,2,2,0],
   [0,2,2,2,0,0,2,0,0,0,2,0,0,2,0],
-  [0,2,2,2,0,0,2,0,0,2,2,2,2,2,0],
+  [0,2,2,2,0,3,2,0,0,2,2,2,2,2,1],
   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 ]
 
@@ -47,27 +47,19 @@ function dibujaEscenario(){
       ctx.fillRect(x*anchoF,y*altoF,anchoF,altoF);
     }
   }
-
-
 }
-
-
-
-
 
 //OBJETO JUGADOR
 var jugador = function(){
   this.x = 1;
   this.y = 1;
-
   this.color = '#820c01';
-
+  this.llave = false;
 
   this.dibuja = function(){
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x*anchoF,this.y*altoF,anchoF,altoF);
   }
-
 
   this.margenes = function(x,y){
     var colision = false;
@@ -79,35 +71,59 @@ var jugador = function(){
     return(colision);
   }
 
-
-
   this.arriba = function(){
-    if(this.margenes(this.x, this.y-1)==false)
+    if(this.margenes(this.x, this.y-1)==false){
       this.y--;
+      this.logicaObjetos();
+    }
   }
 
-
   this.abajo = function(){
-    if(this.margenes(this.x, this.y+1)==false)
+    if(this.margenes(this.x, this.y+1)==false){
       this.y++;
+      this.logicaObjetos();
+    }
   }
 
   this.izquierda = function(){
-    if(this.margenes(this.x-1, this.y)==false)
+    if(this.margenes(this.x-1, this.y)==false){
       this.x--;
+      this.logicaObjetos();
+    }
   }
 
   this.derecha = function(){
-    if(this.margenes(this.x+1, this.y)==false)
+    if(this.margenes(this.x+1, this.y)==false){
       this.x++;
+      this.logicaObjetos();
+    }
   }
 
+  this.logicaObjetos = function() {
+    let objeto = escenario[this.y][this.x];
+    if (objeto === 3) {
+      this.llave = true;
+      escenario[this.y][this.x] = 2;
+      alert("¡Llave obtenida!");
+    }
+    if (objeto === 1) {
+      if (!this.llave) {
+        this.x--;
+        alert("Necesitas encontrar la llave");
+      } else {
+        this.victoria();
+      }
+    }
+  }
+
+  this.victoria = function() {
+    alert("¡Has ganado!");
+    this.x = 1;
+    this.y = 1;
+    this.llave = false;
+    escenario[8][5] = 3;
+  }
 }
-
-
-
-
-
 
 function inicializa(){
   canvas = document.getElementById('canvas');
@@ -118,23 +134,21 @@ function inicializa(){
 
   //LECTURA DEL TECLADO
   document.addEventListener('keydown',function(tecla){
+    if(tecla.keyCode == 37){
+      protagonista.izquierda();
+    }
 
     if(tecla.keyCode == 38){
       protagonista.arriba();
+    }
+    
+    if(tecla.keyCode == 39){
+      protagonista.derecha();
     }
 
     if(tecla.keyCode == 40){
       protagonista.abajo();
     }
-
-    if(tecla.keyCode == 37){
-      protagonista.izquierda();
-    }
-
-    if(tecla.keyCode == 39){
-      protagonista.derecha();
-    }
-
   });
 
   setInterval(function(){
@@ -142,12 +156,10 @@ function inicializa(){
   },1000/FPS);
 }
 
-
 function borraCanvas(){
   canvas.width=750;
   canvas.height=500;
 }
-
 
 function principal(){
   borraCanvas();
